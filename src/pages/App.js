5946ed325a0313ac4cdecbcb6c88f40649e06b88
naberin <aberinnj@gmail.com>
2021-12-21 00:17:14 -0600

    Moved App files to pages

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
+import AppHeader from "../components/header";
 function App() {
   return (
+
     <div className="App">
+      <AppHeader title="Depth"/>
+
     </div>
   );
 }
