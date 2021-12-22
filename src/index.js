45c3226dbcedfdbc69259b13749bcaf3e256b65f
naberin <aberinnj@gmail.com>
2021-12-21 21:29:06 -0600

    Moved App to its own directory and added Sidebar to it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
+import App from './pages/App/App';
 ReactDOM.render(
   <BrowserRouter>
       <Routes>
---
+++ src/pages/App/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {Routes} from "react-router-dom";
+
+import AppHeader from "../../components/header";
+import Sidebar from "../../components/sidebar/sidebar";
+function App() {
+  return (
+
+    <div className="app flex flex-row">
+        <Sidebar />
+        <AppHeader title="Depth"/>
+
+        <Routes>
+
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
