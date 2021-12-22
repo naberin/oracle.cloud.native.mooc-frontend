1b7de46fff8b6ec75e2a7b2d78a1411cf67264d1
naberin <aberinnj@gmail.com>
2021-12-21 21:27:02 -0600

    Added react-router with basic routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <Routes>
+          <Route path="*" element={<App />} />
+      </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
